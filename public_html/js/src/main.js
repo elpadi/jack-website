753fcd9599654e16dd903e4e18c3301ebc1e9247
carlos (el-cajon) <padi_05@yahoo.com>
2013-11-23 02:14:40 -0500

    Set up the core page switching code.

--- public_html/js/src/main.js
+++ public_html/js/src/main.js
@@ -7,46 +7,52 @@ require.config({
 	}
 });
 
-function Magazine($mag) {
-	var _this = this;
-	this.$mag = $mag;
-	this.$posters = $mag.find('.magazine__section');
-}
+require(['jquery','lib/ui/SectionSwitcher','lib/ui/SectionSwitcher/ArrowNav','lib/ui/SectionSwitcher/SectionLinks'], function(jquery, SectionSwitcher, ArrowNav, SectionLinks) {
 
-Magazine.prototype.currentIndex = 0;
-Magazine.prototype.isFlipped = false;
-
-Magazine.prototype.toggleOpen = function(index) {
-	this.$posters.eq(index).toggleClass('open');
-};
-
-Magazine.prototype.open = function(index) {
-	this.$posters.eq(index).addClass('open');
-};
-
-Magazine.prototype.close = function(index) {
-	this.$posters.eq(index).removeClass('open');
-};
-
-Magazine.prototype.flip = function(index) {
-	this.$posters.eq(index).toggleClass('flip');
-	this.isFlipped = true;
-};
-
-Magazine.prototype.reverse = function(index) {
-	var _this = this;
-	if (this.$posters.eq(index).data('close-before-flip')) {
-		this.close();
-		setTimeout(function() { _this.flip(index); }, 2000);
-		setTimeout(function() { _this.open(index); }, 3000);
+	/*
+	function Magazine($mag) {
+		var _this = this;
+		this.$mag = $mag;
+		this.$posters = $mag.find('.magazine__section');
 	}
-	else {
-		this.flip(index);
-	}
-};
 
-require(['jquery'], function(jquery) {
-	window.mag = new Magazine($('.magazine'));
+	Magazine.prototype.currentIndex = 0;
+	Magazine.prototype.isFlipped = false;
+
+	Magazine.prototype.toggleOpen = function(index) {
+		this.$posters.eq(index).toggleClass('open');
+	};
+
+	Magazine.prototype.open = function(index) {
+		this.$posters.eq(index).addClass('open');
+	};
+
+	Magazine.prototype.close = function(index) {
+		this.$posters.eq(index).removeClass('open');
+	};
+
+	Magazine.prototype.flip = function(index) {
+		this.$posters.eq(index).toggleClass('flip');
+		this.isFlipped = true;
+	};
+
+	Magazine.prototype.reverse = function(index) {
+		var _this = this;
+		if (this.$posters.eq(index).data('close-before-flip')) {
+			this.close();
+			setTimeout(function() { _this.flip(index); }, 2000);
+			setTimeout(function() { _this.open(index); }, 3000);
+		}
+		else {
+			this.flip(index);
+		}
+	};
+	*/
+
+	//window.mag = new Magazine($('.magazine'));
+	var mag = new SectionSwitcher($('.magazine'));
+	mag.addComponent(ArrowNav).addComponent(SectionLinks).init();
+	window.mag = mag;
 });
 
 require(['lib/dom/absolute-fixed']);
