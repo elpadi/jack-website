eb70afa285d8caf4adde600be7cec0dbec0284c4
carlos <padi_05@yahoo.com>
2016-03-23 04:27:27 -0400

    Menu color fixes

--- public_html/js/snippets/svg-rollovers.js
+++ public_html/js/snippets/svg-rollovers.js
@@ -1,10 +1,24 @@
-Array.from(document.getElementsByClassName('svg-image')).forEach(function(obj) {
-	obj.parentNode.addEventListener('mouseover', function(e) {
-		obj.contentDocument.querySelector('svg').setAttribute('class', 'over');
+(function() {
+
+	var setSvgClass = function(obj, value, replace) {
+		updateSvg(obj, function() {
+			var svg = obj.contentDocument.querySelector('svg');
+			svg.setAttribute('class', replace ? svg.className.baseVal.replace(replace, value) : svg.className.baseVal + ' ' + value);
+		});
+	};
+
+	var updateSvg = function(obj, fn) {
+		if (obj.contentDocument && obj.contentDocument.readyState === 'complete' && obj.contentDocument.querySelector('svg')) fn();
+		else obj.addEventListener('load', fn);
+	};
+
+	Array.from(document.getElementsByClassName('svg-image')).forEach(function(obj) {
+		obj.parentNode.addEventListener('mouseover', function(e) { setSvgClass(obj, 'over'); });
+		obj.parentNode.addEventListener('mouseout', function(e) { setSvgClass(obj, '', 'over'); });
+		if (obj.parentNode.classList.contains('selected')) setSvgClass(obj, 'selected');
 	});
-	obj.parentNode.addEventListener('mouseout', function(e) {
-		obj.contentDocument.querySelector('svg').setAttribute('class', '');
-	});
-	if (obj.parentNode.classList.contains('selected'))
-		obj.contentDocument.querySelector('svg').setAttribute('class', 'over');
-});
+	Array.from(document.getElementById('masthead').getElementsByClassName('svg-image')).filter(function(obj) {
+		return window.getComputedStyle(obj.parentNode)["background-color"].includes('rgba(0');
+	}).forEach(function(obj) { setSvgClass(obj, 'selected') });
+
+})();
