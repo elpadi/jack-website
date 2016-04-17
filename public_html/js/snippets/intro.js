6029eccff9de212e0b45a2313f172a8030db5fc1
carlos (el-cajon) <padi_05@yahoo.com>
2016-04-17 16:26:00 -0400

    Skip intro by click

--- public_html/js/snippets/intro.js
+++ public_html/js/snippets/intro.js
@@ -1,7 +1,42 @@
+var Intro = {
+
+	skipped: false,
+	timeoutIds: [],
+
+	run: function() {
+		Intro.show('intro-fade');
+		Intro.hideOne('.posters', 5000);
+		Intro.hideOne('.flags', 11000);
+		Intro.hideOne('.logo', 17000);
+	},
+
+	skip: function() {
+		Intro.skipped = true;
+		Intro.timeoutIds.forEach(clearTimeout);
+		document.body.classList.add('skip-intro');
+	},
+
+	show: function(className) {
+		Array.from(document.getElementsByClassName(className)).forEach(function(node) { node.classList.add('visible'); });
+	},
+
+	hide: function(className) {
+		Array.from(document.getElementsByClassName(className)).forEach(function(node) { node.classList.remove('visible'); });
+	},
+
+	hideOne: function(query, delay) {
+		Intro.timeoutIds.push(setTimeout(function() { document.querySelector(query).classList.remove('visible'); }, delay));
+	},
+
+	skipListener: function(e) {
+		if (Intro.skipped) return;
+		Intro.skip();
+		e.currentTarget.removeEventListener(e.type, Intro.skipListener);
+	}
+
+};
+
 window.addEventListener('load', function() {
-	Array.from(document.getElementsByClassName('intro-fade')).forEach(function(node) { node.classList.add('visible'); });
-	Array.from(document.getElementsByClassName('post-intro-fade')).forEach(function(node) { node.classList.add('visible'); });
-	setTimeout(function() { document.querySelector('.posters').classList.remove('visible'); }, 5000);
-	setTimeout(function() { document.querySelector('.flags').classList.remove('visible'); }, 11000);
-	setTimeout(function() { document.querySelector('.logo').classList.remove('visible'); }, 17000);
+	Intro.run();
 });
+document.addEventListener('click', Intro.skipListener);
