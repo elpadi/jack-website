0a92ecc96916ece993760b1aa528d287195b1525
carlos (minimal) <padi_05@yahoo.com>
2017-12-13 20:00:57 -0500

    Moved env vars to .env

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,4 +6,8 @@ define('WEBSITE_DIR', dirname(__DIR__));
 
 require(WEBSITE_DIR.'/vendor/autoload.php');
 require(PUBLIC_ROOT_DIR.'/admin/bootstrap.php');
+
+$env = new Dotenv\Dotenv(WEBSITE_DIR);
+$env->load();
+
 $app = new App();
